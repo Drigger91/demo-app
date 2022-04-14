6016a96b53a225d98dca28ae0bc171c883e821ba
Drigger91 <mepiyusht@gmail.com>
2022-04-15 00:23:08 +0530

    demo-app

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("go to route for data");
+});
+app.post("/route", (req, res) => {
+  const array = req.body.arr;
+  const even = [];
+  const odd = [];
+  let i;
+  for (i = 0; i < array.length; i++) {
+    if (typeof array[i] == "number") {
+      if (array[i] % 2 == 1) {
+        odd.push(array[i]);
+      } else {
+        even.push(array[i]);
+      }
+    }
+  }
+  res
+    .send({
+      even: even,
+      odd: odd,
+    })
+    .json()
+    .status(200);
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("server is up and running");
+});
